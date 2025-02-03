b8933302f066c35738b8254b021544ad9f348846
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-03 23:24:05 +0100

    third commit

--- class-notes/01-SQL_1/sql1.sql
+++ class-notes/01-SQL_1/sql1.sql
@@ -34,3 +34,35 @@ WHERE city='Berlin';
 -- =, !=, <>, >=, <=, BETWEEN
 -- Amerika disindaki müsteriler
 SELECT * FROM customers WHERE Country!='USA';
+-- AND, OR, IN, NOT IN
+-- müsterinin ülkesi Almanya veya Amerika olanlar
+SELECT * FROM customers WHERE Country='USA' OR Country='Germany';
+-- veya IN ile yazabiliriz
+SELECT * FROM customers WHERE Country IN ('USA', 'Germany');
+SELECT * FROM customers WHERE Country NOT IN ('USA', 'Germany'); -- bu iki ülke disindakiler
+SELECT * FROM customers WHERE Country!='USA' AND Country!='Germany';
+-- Fatura tutari 5 ile 10 arasinda olanlar
+SELECT * FROM invoices WHERE total>=5.94 AND total<=10;
+SELECT * FROM invoices WHERE total BETWEEN 5.94 AND 10;
+
+-- like %, _
+-- ismi a ile baslayan müsteriler
+SELECT * FROM customers WHERE FirstName LIKE 'b%';
+-- ismi s ile biten müsteriler
+SELECT * FROM customers WHERE FirstName LIKE '%s';
+SELECT * FROM customers WHERE FirstName LIKE '1%s'; -- L ile baslasin s ile bitsin
+SELECT * FROM customers WHERE FirstName LIKE '1_c%';
+SELECT * FROM customers WHERE FirstName LIKE '1_c_';
+
+-- ORDER BY SIRALAMA (ASC, DESC)
+SELECT * FROM customers ORDER BY FirstName ASC;
+SELECT * FROM customers ORDER BY FirstName DESC;
+SELECT * FROM customers ORDER BY FirstName; -- default ASC
+SELECT * FROM customers ORDER BY FirstName,LastName DESC;
+
+-- DISTINCT
+-- her ülkeyi tek bir defa getir
+SELECT DISTINCT(Country) FROM customers;
+-- count
+-- kac farkli ülkeden müsterim var
+SELECT Count (DISTINCT(Country)) FROM customers;
