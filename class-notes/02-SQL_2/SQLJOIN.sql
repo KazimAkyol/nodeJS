3b1194e933a6bf655c89b2f20a7accbfcc699b3c
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-04 17:09:57 +0100

    fifth commit

--- class-notes/02-SQL_2/SQLJOIN.sql
+++ class-notes/02-SQL_2/SQLJOIN.sql
@@ -14,3 +14,39 @@ SELECT CustomerId,min(total),max(total),avg(total),sum(total),count(total) FROM 
 SELECT CustomerId, min(total) as en_dusuk_fatura FROM invoices;
 
 -- SUB QUERY (nested)
+-- ortalama sürenin üzerindeki track'lar
+-- ortalama süre
+SELECT avg(Milliseconds) FROM tracks;
+
+SELECT * FROM tracks WHERE Milliseconds>400000;
+SELECT * FROM tracks WHERE Milliseconds>(SELECT avg(Milliseconds) FROM tracks);
+
+-- ortalama faturadan yüksek olan fatura sayisi
+SELECT avg(total) FROM invoices;
+SELECT * FROM invoices WHERE total>(SELECT avg(total) FROM invoices);
+SELECT count(*) FROM invoices WHERE total(SELECT avg(total) FROM invoices);
+
+SELECT *
+FROM invoices
+WHERE total>(SELECT avg(total) FROM invoices);
+--WHERE total>10;
+
+-- 'Go Down' müzik parcasi hangi albümde
+SELECT * FROM tracks WHERE Name='Go Down';
+SELECT AlbumId from albums WHERE Name='Go Down';
+SELECT * FROM albums WHERE AlbumId=(SELECT AlbumId FROM tracks WHERE Name='Go Down');
+
+-- 'Let There Be Rock' albümünde hangi parcalar var
+SELECT * FROM tracks WHERE AlbumId=4;
+SELECT AlbumId FROM albums WHERE Title='Let There Be Rock';
+SELECT *
+FROM tracks
+WHERE AlbumId=(SELECT AlbumId
+               FROM albums
+               WHERE Title='Let There Be Rock');
+
+-- Berlin'de fatura kesilen müsterilerin ad,soyad ve telefonu
+SELECT CustomerId FROM invoices WHERE BillingCity='Berlin';
+SELECT * FROM customers WHERE CustomerId=38 or CustomerId=36;
+--veya
+SELECT * FROM customers
