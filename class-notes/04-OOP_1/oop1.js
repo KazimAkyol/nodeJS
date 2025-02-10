127b58f70d5eced4895072637da4a406e84c2ef8
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-10 10:33:52 +0100

    12.commit

--- class-notes/04-OOP_1/oop1.js
+++ class-notes/04-OOP_1/oop1.js
@@ -86,17 +86,58 @@ const Car = {
 // console.log(Car.getDetails());
 console.log(Car.getDetailsWithArrowFn()); // This keyword arrow fonksiyonlarda ulasilamaz.
 
-/*------------------------------------------*/
+/*------------------------------------------*
 
 //? ARRAY DESTRUCTIONS
 
 const testArr = ["value0", "value1", "value2", "value3"];
 
-console.log(testArr[0]);
-console.log(testArr[1]);
-console.log(testArr[2]);
+// console.log(testArr[0]);
+// console.log(testArr[1]);
+// console.log(testArr[2]);
+// const val3 = testArr.slice(3, 4);
+// console.log(val3);
+
+// siralama önemli
+// rest operatörü her zaman en son kullanilir.
+const [item1, item2, ...otherItems] = testArr;
+
+console.log(item1);
+console.log(otherItems);
 
 /*------------------------------------------*/
+//? OBJECT DESTRUCTURING
+
+const Car = {
+  brand: "Mercedes",
+  model: "Maybach",
+  year: 2025,
+  isAutoGear: true,
+  colors: ["black", "red"],
+  details: {
+    color1: "black",
+    color2: "red",
+    engineSize: 5000,
+  },
+  startEngine: function () {
+    console.log(this.brand);
+    return "Engine started";
+  },
+  getDetails: function () {
+    return this.details;
+  },
+  getDetailsWithArrowFn: () => {
+    return this.details;
+  },
+};
+
+// Rest Operator (key isimleri önemli)
+
+const { brand, ...otherItems } = Car;
+
+console.log(brand);
+console.log(otherItems);
+
 /*------------------------------------------*/
 /*------------------------------------------*/
 /*------------------------------------------*/
