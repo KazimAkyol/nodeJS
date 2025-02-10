6a357c6bfc5234b8edb4e9548b1da67047b3ad45
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-10 14:10:10 +0100

    15.commit

---
+++ class-notes/03-jsRecap_NodejsSetup/index.js
@@ -0,0 +1,21 @@
+"use strict";
+// $ node
+// console.log("terminal");
+
+// npm install --global nodemon
+// nodemon --version
+
+console.log("Nodemon ile js calistiriyoruz");
+
+let num1 = 5;
+console.log(typeof num1);
+let num2 = 14;
+let toplam = num1 + num2;
+console.log(toplam);
+console.log(`${num1 + num2}`);
+
+console.log("for döngüsü");
+
+for (let i = 1; i < 5; i++) {
+  console.log(`Sayi: ${i}`);
+}
