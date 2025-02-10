fe08d38014c904d0c69b905bae294a2313fbdadb
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-10 16:38:25 +0100

    16.commit

--- class-notes/03-jsRecap_NodejsSetup/index.js
+++ class-notes/03-jsRecap_NodejsSetup/index.js
@@ -19,3 +19,14 @@ console.log("for döngüsü");
 for (let i = 1; i < 5; i++) {
   console.log(`Sayi: ${i}`);
 }
+
+function carpma(a, b) {
+  return a * b;
+}
+
+console.log(`${carpma(2, 10)}`);
+console.log(typeof `${carpma(2, 10)}`);
+
+const result = carpma(2, 8);
+console.log(result);
+console.log(typeof result);
