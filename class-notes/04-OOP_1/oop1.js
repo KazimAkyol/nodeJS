e8ae3caf820412b88c3db069b4703f4e68ca91bd
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-10 09:58:56 +0100

    11.commit

--- class-notes/04-OOP_1/oop1.js
+++ class-notes/04-OOP_1/oop1.js
@@ -20,27 +20,82 @@ const exampleObject = {
 console.log(exampleObject.propertyName);
 console.log(exampleObject.methodName());
 
-/*------------------------------------------*/
+/*------------------------------------------*
 
 const Car = {
-    brand: 'Mercedes',
-    model: 'Maybach',
-    year: 2025,
-    isAutoGear: true,
-    color: ['black', 'red'],
-    details: {
-        color1:'black',
-        color2: 'red',
-        engineSize: 5000
-    },
-    startEngine: function() {
-        return 'Engine started'
-    }
-}
+  brand: "Mercedes",
+  model: "Maybach",
+  year: 2025,
+  isAutoGear: true,
+  colors: ["black", "red"],
+  details: {
+    color1: "black",
+    color2: "red",
+    engineSize: 5000,
+  },
+  startEngine: function () {
+    return "Engine started";
+  },
+};
 
+/*------------------------------------------*
+
+console.log(Car);
+console.log(Car.brand);
+console.log(Car.model);
+console.log(Car.year);
+console.log(Car.isAutoGear);
+console.log(Car.colors);
+console.log(Car.details);
+console.log(Car.startEngine());
+
+// Alternative:
+
+console.log(Car["brand"]);
+console.log(Car["colors"]);
+console.log(Car["colors"][0]);
+console.log(Car["details"].engineSize);
+console.log(Car["details"][engineSize]);
+
+/*------------------------------------------*
+//? THIS KEYWORD
+
+const Car = {
+  brand: "Mercedes",
+  model: "Maybach",
+  year: 2025,
+  isAutoGear: true,
+  colors: ["black", "red"],
+  details: {
+    color1: "black",
+    color2: "red",
+    engineSize: 5000,
+  },
+  startEngine: function () {
+    console.log(this.brand);
+    return "Engine started";
+  },
+  getDetails: function () {
+    return this.details;
+  },
+  getDetailsWithArrowFn: () => {
+    return this.details;
+  },
+};
+
+// console.log(Car.getDetails());
+console.log(Car.getDetailsWithArrowFn()); // This keyword arrow fonksiyonlarda ulasilamaz.
 
 /*------------------------------------------*/
-/*------------------------------------------*/
+
+//? ARRAY DESTRUCTIONS
+
+const testArr = ["value0", "value1", "value2", "value3"];
+
+console.log(testArr[0]);
+console.log(testArr[1]);
+console.log(testArr[2]);
+
 /*------------------------------------------*/
 /*------------------------------------------*/
 /*------------------------------------------*/
