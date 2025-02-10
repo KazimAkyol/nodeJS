94fe7f994dc936e2841c781538573018559f0e7c
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-10 09:38:56 +0100

    10.commit

--- class-notes/04-OOP_1/oop1.js
+++ class-notes/04-OOP_1/oop1.js
@@ -1,30 +1,44 @@
-'use strict';
+"use strict";
 
-/* ---------------------------------------- */ // alt + shift + a
+/* ---------------------------------------- * // alt + shift + a
                     OBJECTS
-/* ---------------------------------------- */
+/* ---------------------------------------- *
 
 // Direkt objeleri isimlendirirken PascalCase veya camelCase kullanabiliriz.
 const exampleObject = {
-    propertyName: "value", // attribute, field
+  propertyName: "value", // attribute, field
 
-    methodName: function () {
-        return 'methodName() is a method';
+  methodName: function () {
+    return "methodName() is a method";
+  },
+
+  methodAlternative() {
+    return "methodAlternative is a method";
+  },
+};
+
+console.log(exampleObject.propertyName);
+console.log(exampleObject.methodName());
+
+/*------------------------------------------*/
+
+const Car = {
+    brand: 'Mercedes',
+    model: 'Maybach',
+    year: 2025,
+    isAutoGear: true,
+    color: ['black', 'red'],
+    details: {
+        color1:'black',
+        color2: 'red',
+        engineSize: 5000
     },
-
-    methodAlternative() {
-        return 'methodAlternative is a method'
+    startEngine: function() {
+        return 'Engine started'
     }
 }
 
 
-
-
-
-
-
-
-/*------------------------------------------*/
 /*------------------------------------------*/
 /*------------------------------------------*/
 /*------------------------------------------*/
