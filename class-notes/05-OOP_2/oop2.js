91cf60a22b9cd44797839af36f7931b1ac63b127
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-11 09:31:33 +0100

    update oop2

--- class-notes/05-OOP_2/oop2.js
+++ class-notes/05-OOP_2/oop2.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /* ------------------------------------ *
             OOP & CLASSES
@@ -8,9 +8,31 @@
 //? BLUEPRINT: Taslak(Mimarlarin kullandigi mavi sablon kagidi)
 //? CLASS: Obje türetmek icin kullanilacak sablon
 
+//* Class Expression
+// const PascalCaseClassName = class { ...}
 
+//* Class Declaration
 
+class PascalCaseClassName {
+  propertyName = "value"; // attribute, field
+  undefinedProperty;
 
+  constructor(parametre1, parametre2 = "default-value") {
+    this.parametre1 = parametre1;
+    this.parametre2 = parametre2;
+  }
+
+  methodName() {
+    return this;
+  }
+}
+
+//* INSTANCE = Bir Class'tan türetilen objedir
+
+const instanceExp = new PascalCaseClassName(0, 1);
+const instanceExp2 = new PascalCaseClassName(0);
+// console.log(instanceExp);
+console.log(instanceExp2);
 
 /* ------------------------------------ */
 /* ------------------------------------ */
