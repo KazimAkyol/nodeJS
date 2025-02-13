720ffc21d78eebaa0d732b3fd2a93c9cd1c85af9
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-13 10:13:59 +0100

    update module

--- class-notes/06-NodeJS_Modules/index.js
+++ class-notes/06-NodeJS_Modules/index.js
@@ -6,5 +6,15 @@ console.log("indexjs");
 
 require("./module/module.js");
 // require("./module/module");
-const testFunc = require("./module/module.js");
-testFunc();
+
+//! tek fonksiyonu import etme
+// const testFunc = require("./module/module.js");
+// testFunc();
+
+// const { testFunc1, testFunc2 } = require("./module/module.js");
+// testFunc1();
+// testFunc2();
+// console.log(testFunc1);
+
+const singleFunc = require("./module/module.js");
+singleFunc();
--- class-notes/06-NodeJS_Modules/module/module.js
+++ class-notes/06-NodeJS_Modules/module/module.js
@@ -2,9 +2,27 @@
 
 console.log("modul dosyasi calisti");
 
-const testFunc = function () {
-  console.log("this is a function");
+//! tek fonksiyonu disari aktarma
+// const testFunc = function () {
+//   console.log("this is a function");
+// };
+
+// module.exports = testFunc;
+
+const singleFunc = function () {
+  const yil = 2025;
+  console.log(yil);
+
+  const testFunc1 = function () {
+    console.log("testFunc1 running");
+  };
+
+  const testFunc2 = function () {
+    console.log("testFunc2 running");
+  };
+
+  testFunc1();
+  testFunc2();
 };
 
-//? Bu fonksiyonu disari aktarmak istiyorum:
-module.exports = testFunc;
+module.exports = singleFunc;
