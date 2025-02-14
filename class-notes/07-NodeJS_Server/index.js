6a9adcf14f42c0e562914396b995ed9e3b1d4f94
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-14 23:08:58 +0100

    add req and res

--- class-notes/07-NodeJS_Server/index.js
+++ class-notes/07-NodeJS_Server/index.js
@@ -1,3 +1,17 @@
-"use strict"
+"use strict";
 
 //! $ npm install dotenv
+
+require("dotenv").config(); //? .env dosyasindaki verileri process.env icine yükler.
+
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || "127.0.0.1";
+
+const http = require("node:http"); // http modülünü cagirir. Bu node.j'in icindedir.
+
+const app = http.createServer((req, res) => {
+  console.log(req); // gelen HTTP istegini tüm detaylariyla yazdirir.
+  console.log("--------------------");
+});
+
+app.listen(PORT, () => console.log(`server running: http://${HOST}:${PORT}`));
