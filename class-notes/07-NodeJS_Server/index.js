a6be2404411a2e3a57b41a2d8df819e891a55e53
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-15 22:53:54 +0100

    add server

--- class-notes/07-NodeJS_Server/index.js
+++ class-notes/07-NodeJS_Server/index.js
@@ -7,11 +7,12 @@ require("dotenv").config(); //? .env dosyasindaki verileri process.env icine yü
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "127.0.0.1";
 
-const http = require("node:http"); // http modülünü cagirir. Bu node.j'in icindedir.
+const http = require("node:http"); // http modülünü cagirir. Bu node.js'in icindedir.
 
 const app = http.createServer((req, res) => {
+  //? Piyasa standarti app olarak isimlendirilir.
   console.log(req); // gelen HTTP istegini tüm detaylariyla yazdirir.
   console.log("--------------------");
 });
 
-app.listen(PORT, () => console.log(`server running: http://${HOST}:${PORT}`));
+app.listen(PORT, () => console.log(`server running: http://${HOST}:${PORT}`)); // Belirtilen HOST ve POST üzerinden sunucuyu baslatir.
