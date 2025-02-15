769eb8872530b8221145caf70722adb8a320fc8e
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-15 23:09:19 +0100

    update server

--- class-notes/07-NodeJS_Server/index.js
+++ class-notes/07-NodeJS_Server/index.js
@@ -4,15 +4,32 @@
 
 require("dotenv").config(); //? .env dosyasindaki verileri process.env icine yükler.
 
-const PORT = process.env.PORT || 8000;
-const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process?.env.PORT || 8000;
+const HOST = process?.env.HOST || "127.0.0.1";
 
 const http = require("node:http"); // http modülünü cagirir. Bu node.js'in icindedir.
 
 const app = http.createServer((req, res) => {
-  //? Piyasa standarti app olarak isimlendirilir.
+  // Piyasa standarti app olarak isimlendirilir.
   console.log(req); // gelen HTTP istegini tüm detaylariyla yazdirir.
   console.log("--------------------");
+  //   if (req.url == "/") {
+  //     res.end("<h1>welcome</h1>");
+  //   } else if (req.url == "/blogs") {
+  //     res.end("<h1>Listelenen Bloglar</h1");
+  //   } else if (req.url == "/newBlog") {
+  //     res.end("<h1>Yeni blog ekle</h1>");
+  //   }
+
+  if (req.url == "/") {
+    res.write("this");
+    res.write("is");
+    res.write("home");
+    res.write("page");
+    res.write("   ");
+    res.write("welcome to clarusway");
+  }
+  res.end();
 });
 
 app.listen(PORT, () => console.log(`server running: http://${HOST}:${PORT}`)); // Belirtilen HOST ve POST üzerinden sunucuyu baslatir.
