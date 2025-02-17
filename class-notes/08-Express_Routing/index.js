adbcad59bc19cbd831dd52ab114a98dc9c3faff7
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-17 10:33:02 +0100

    add crud

--- class-notes/08-Express_Routing/index.js
+++ class-notes/08-Express_Routing/index.js
@@ -16,6 +16,17 @@ const app = express(); // Run application on express.
 require("dotenv").config();
 const PORT = process.env.PORT;
 
+// if(req.url == "/") { //*Routing with NodeJs
+
+// }
+
+//* HTTP_METHODS & URLS:
+app.get("/", (req, res) => res.end("called in 'get' method."));
+app.post("/", (req, res) => res.end("called in 'post' method."));
+app.put("/", (req, res) => res.end("called in 'put' method."));
+app.patch("/", (req, res) => res.end("called in 'patch' method."));
+app.delete("/", (req, res) => res.end("called in 'delete' method."));
+
 /* ------------------------------------------------------------- */
 
 app.listen(PORT, () => console.log("Running at: http://127.0.0.1:" + PORT));
