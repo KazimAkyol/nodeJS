567c170817f5c87588ebb60a6d82d0c72e67cbe3
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-18 23:52:25 +0100

    update node-modules

--- class-notes/06-NodeJS_Modules/index.js
+++ class-notes/06-NodeJS_Modules/index.js
@@ -1,12 +1,12 @@
 "use strict";
 
-// $npm init -y
+// $ npm init -y
 // npm install // package-lock.json olusturur.
 // nodemon // nodemon index.js'de calistirir ve izler.
 
 console.log("indexjs");
 
-//? baska bir js dosyasini iceri aktarma
+//? baska bir js dosyasini iceri aktarma:
 
 require("./module/module.js");
 // require("./module/module");
