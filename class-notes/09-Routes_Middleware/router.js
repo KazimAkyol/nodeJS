f6700a21ea74f621d11dbe8c94e363274eb26722
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-18 09:37:31 +0100

    update router

--- class-notes/09-Routes_Middleware/router.js
+++ class-notes/09-Routes_Middleware/router.js
@@ -8,11 +8,28 @@
 // $ npm i express dotenv
 // $ .gitignore dosyasini gitignore.io'ya Node yazarak oradaki kodlari alip klasördeki .gitignore dosyasina yapistirilir.
 // $ echo PORT=8000 > .env => yazarak klasöre otomatik olarak .env dosyasi olusturulur.
+/* --------------------------------------------------------- */
 
 const express = require("express");
 const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT;
+/* --------------------------------------------------------- */
+//? "Router" is special app for URL control in ExpressJS.
+
+// app.get("/", (req, res) => { // app-route
+//   res.send({
+//     message: "Merhaba Cohort DE-10",
+//   });
+// });
+
+const router = express.Router();
+
+router.get("/", (req, res) => res.send({ message: "Home Page" }));
+router.get("/about", (req, res) => res.send({ message: "About Page" }));
+router.get("/user/:id", (req, res) => res.send({ message: req.params.id }));
+
+app.use(router);
 
 app.listen(PORT, () => console.log("Running at: http://127.0.0.1:" + PORT));
