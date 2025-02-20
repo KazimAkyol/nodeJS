9b3b3008b9029b06d4c651c86cf0bce77f7037ba
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-20 11:17:48 +0100

    update builtin

--- class-notes/10-Error_Management/builtin.js
+++ class-notes/10-Error_Management/builtin.js
@@ -1,4 +1,5 @@
 "use strict";
+const { error } = require("console");
 /* -------------------------------------------------------
     EXPRESSJS - BUILTIN MIDDLEWARES
 ------------------------------------------------------- */
@@ -11,6 +12,25 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 
+//? Data receiving
+//* Read & Parse json data
+app.use(express.json());
+
+//* Read text data
+app.use(express.text());
+
+//* Read form data
+app.use(express.urlencoded({ extended: true }));
+
+app.all("/:id", (req, res) => {
+  res.send({
+    error: false,
+    params: req.params,
+    query: req.query,
+    headers: req.headers,
+    body: req.body,
+  });
+});
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
