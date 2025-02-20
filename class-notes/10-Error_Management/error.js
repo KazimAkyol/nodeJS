6829e16ef09c855e5cef7644210c682c51162c92
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-20 10:29:34 +0100

    add error-management

---
+++ class-notes/10-Error_Management/error.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/* -------------------------------------------------------
+    EXPRESSJS - ERROR MANAGEMENT
+------------------------------------------------------- */
+
+// $ npm init -y
+// npm install express dotenv
+// echo PORT=800 > .env
+// cat > .gitignore && paste gitignore content & ctrl+c
+// nodemon
+
