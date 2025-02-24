8181fb4427d77251b6c9a217405cc3c1c5205d0d
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-24 11:31:57 +0100

    add controller

--- class-notes/12-Todo_Sequelize_2/controllers/todo.controller.js
+++ class-notes/12-Todo_Sequelize_2/controllers/todo.controller.js
@@ -6,22 +6,83 @@
 
 //* Controllers:
 
+const Todo = require("../models/todo.model");
+
 module.exports = {
-    list: async (req, res) => {
-        const result = await Todo.findAll(); // Select * From todos
-        // findAndCountAll()
-      
-        res.send({
-          error: false,
-          result,
-        });
-      } ,
+  list: async (req, res) => {
+    const result = await Todo.findAll(); // Select * From todos
+    // findAndCountAll()
 
-    create: ,
+    res.send({
+      error: false,
+      result,
+    });
+  },
 
-    read: ,
+  create: async (req, res) => {
+    // const result = await Todo.create({
+    //     title: "title-1",
+    //     description: 'description-1',
+    //     isDone: false,
+    //     priority: 0
+    // } ,
 
-    update: ,
+    const result = await Todo.create(req.body);
 
-    delete: ,
-}
+    res.status(201).send({
+      error: false,
+      result,
+    });
+  },
+
+  read: async (req, res) => {
+    //   const result = await Todo.findOne({ where: { id: req.params.id } });
+
+    const result = await Todo.findByPk(req.params.id);
+
+    res.status(202).send({
+      error: false,
+      result,
+    });
+  },
+
+  update: async (req, res) => {
+    //   const result = await Todo.update({ ...newData }, { ...where }); => [1]:success, [0]:failed
+    //   const result = await Todo.update(
+    //     { title: "title-updated-5" },
+    //     { where: { id: 10 } }
+    //   );
+    const result = await Todo.update(
+      { title: req.body },
+      { where: { id: req.params.id } }
+    );
+
+    res.status(202).send({
+      error: false,
+      result,
+      new: await Todo.findByPk(req.params.id),
+    });
+  },
+
+  delete: async (req, res) => {
+    // await Todo.destroy({ ...where }); => 1:success, 0:failed
+    const result = await Todo.destroy({ where: { id: req.params.id } });
+
+    // 204 status code does not display and response
+    //   res.status(204).send({
+    //     error: false,
+    //     result,
+    //   });
+
+    if (result) {
+      // successfull
+      res.sendStatus(204);
+    } else {
+      // failed
+      res.statusCode = 404;
+      throw new Error(
+        "Delete is not successful. Data is not found or already deleted."
+      );
+    }
+  },
+};
--- class-notes/12-Todo_Sequelize_2/routes/todo.router.js
+++ class-notes/12-Todo_Sequelize_2/routes/todo.router.js
@@ -9,88 +9,28 @@ const { Router } = require("express");
 //* Routers
 
 const router = require("express").Router();
+const todo = require("../controllers/todo.controller");
 
-//* Model
+// //* List Todos:
+// router.get("/todo", todo.list);
 
-require("../models/todo.model");
+// //* Create Todo:
+// router.post("/todo", todo.create);
 
-// List Todos:
-router.get("/todo", );
+// //* Read Todo:
 
-// Create Todo:
-router.post("/todo", async (req, res) => {
-  // const result = await Todo.create({
-  //     title: "title-1",
-  //     description: 'description-1',
-  //     isDone: false,
-  //     priority: 0
-  // });
+// router.get("/todo/:id", todo.read);
 
-  const result = await Todo.create(req.body);
+// //* Update Todo:
 
-  res.status(201).send({
-    error: false,
-    result,
-  });
-});
+// router.put("/todo/:id", todo.update);
 
-//* TODO => READ - UPDATE - DELETE
+// //* Delete Todo:
 
-//* Read Todo:
+// router.delete("/todo/:id", todo.delete);
 
-router.get("/todo/:id", async (req, res) => {
-  //   const result = await Todo.findOne({ where: { id: req.params.id } });
+router.route("todo").get(todo.list).post(todo.create);
 
-  const result = await Todo.findByPk(req.params.id);
-
-  res.status(202).send({
-    error: false,
-    result,
-  });
-});
-
-//* Update Todo:
-
-router.put("/todo/:id", async (req, res) => {
-  //   const result = await Todo.update({ ...newData }, { ...where }); => [1]:success, [0]:failed
-  //   const result = await Todo.update(
-  //     { title: "title-updated-5" },
-  //     { where: { id: 10 } }
-  //   );
-  const result = await Todo.update(
-    { title: req.body },
-    { where: { id: req.params.id } }
-  );
-
-  res.status(202).send({
-    error: false,
-    result,
-    new: await Todo.findByPk(req.params.id),
-  });
-});
-
-//* Delete Todo:
-
-router.delete("/todo/:id", async (req, res) => {
-  // await Todo.destroy({ ...where }); => 1:success, 0:failed
-  const result = await Todo.destroy({ where: { id: req.params.id } });
-
-  // 204 status code does not display and response
-  //   res.status(204).send({
-  //     error: false,
-  //     result,
-  //   });
-
-  if (result) {
-    // successfull
-    res.sendStatus(204);
-  } else {
-    // failed
-    res.statusCode = 404;
-    throw new Error(
-      "Delete is not successful. Data is not found or already deleted."
-    );
-  }
-});
+router.route("/todo/id").get(todo.read).put(todo.update).delete(todo.delete);
 
 module.exports = router;
