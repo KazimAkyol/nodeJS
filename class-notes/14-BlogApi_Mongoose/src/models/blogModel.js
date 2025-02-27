4082f1aaef08c2b58eb31dca7d52e9786039661b
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-27 10:53:26 +0100

    update blogModel

--- class-notes/14-BlogApi_Mongoose/src/models/blogModel.js
+++ class-notes/14-BlogApi_Mongoose/src/models/blogModel.js
@@ -1,3 +1,29 @@
-"use strict"
+"use strict";
 
 /* --- EXPRESSJS */
+
+const mongoose = require("mongoose");
+
+// const nameSchema = new mongoose.Schema({ fields }, { options });
+
+const nameSchema = new mongoose.Schema({
+    // _id: // auto created and increment
+
+    fieldName: {
+        type: String,
+        default: null,
+        trim: true,
+        unique: true,
+        select: false, // if we don't want to send this field.
+        index:true,
+        // required: true,
+        required: [true, 'custom error message'],
+        // enum: ['1', '2', '3'],
+        // enum: [1,2,3],
+        enum: [[1,2,3], 'custom error message'],
+        // validate: ()=>true, // if return false it will throw a validation error.
+        validate: [()=> true,'cutom error message'],
+        get: () => {return data}, // it works default when we do read operation
+        set: ()=> {return data}, // it works default when we do create or update operation
+    }
+},{})
