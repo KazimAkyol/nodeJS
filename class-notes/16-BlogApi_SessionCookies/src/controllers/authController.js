7344e9f7544928114bde09d5810e4de19c5a6f52
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-03 09:59:23 +0100

    add findOne method

--- class-notes/16-BlogApi_SessionCookies/src/controllers/authController.js
+++ class-notes/16-BlogApi_SessionCookies/src/controllers/authController.js
@@ -10,8 +10,18 @@ module.exports = {
     const { email, password } = req.body;
 
     if (email && password) {
+      const user = await User.findOne({ email, password }); // findOne runs set method as a default
 
-      const user = await User.findOne({email: email})
+      if (user) {
+        res.send({
+          error: false,
+          message: "login OK",
+          User, // Response'da User'a ait bilgilere ulasilir.
+        });
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("User credentials are wrong.");
+      }
 
       res.send({
         error: false,
