dbfbe0b536ec1ce2540b4204bb639506db09d614
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-04 12:37:51 +0100

    add passwordEncrypt

--- class-notes/17-BlogApi_SearchSortPagination/src/models/userModel.js
+++ class-notes/17-BlogApi_SearchSortPagination/src/models/userModel.js
@@ -5,7 +5,9 @@
 
 const mongoose = require("mongoose");
 
-// Password Encryption:
+/* -------------------------------------------------------*
+
+//* Password Encryption:
 // https://nodejs.org/docs/latest/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
 
 const crypto = require("node:crypto");
