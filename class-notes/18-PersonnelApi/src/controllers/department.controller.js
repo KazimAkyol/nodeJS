04f8da3d7b6e0f99b2a51f57bfa17f92b7e3da5f
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-06 10:21:05 +0100

    add department controller

--- class-notes/18-PersonnelApi/src/controllers/department.controller.js
+++ class-notes/18-PersonnelApi/src/controllers/department.controller.js
@@ -2,3 +2,48 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = require('../models/department.model')
+
+module.exports = {
+
+    list: async (req,res) => {
+
+
+        res.status().send({
+            error: false,
+        })
+    },
+
+    create: async (req,res) => {
+
+
+        res.status().send({
+            error: false,
+        })
+    },
+
+    read: async (req,res) => {
+
+
+        res.status().send({
+            error: false,
+        })
+    },
+
+    update: async (req,res) => {
+
+
+        res.status().send({
+            error: false,
+        })
+    },
+
+    delete: async (req,res) => {
+
+
+        res.status().send({
+            error: false,
+        })
+    },
+}
