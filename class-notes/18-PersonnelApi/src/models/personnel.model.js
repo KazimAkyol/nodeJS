94f68aa2714ca31ceb7b00531aba78ad0fd5fbe2
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-06 11:49:56 +0100

    updated personnelmodel.js

--- class-notes/18-PersonnelApi/src/models/personnel.model.js
+++ class-notes/18-PersonnelApi/src/models/personnel.model.js
@@ -1,49 +1,100 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const { validate } = require("./department.model");
 /* ------------------------------------------------------- */
 
 const PersonnelSchema = new mongoose.Schema({
+  departmentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Department",
+    required: true,
+  },
 
-    departmentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Department',
-        required: true,
-    },
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
 
-    username: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-    },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    // set: (password) => passwordEncrypt(password),
+    set: passwordEncrypt,
+  },
 
-    password: {
-        type: String,
-        trim: true,
-        required: true,
-        // set: (password) => passwordEncrypt(password),
-        set: passwordEncrypt
-    },
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
 
-    firstName: {
-        type: String,
-        trim: true,
-        required: true
-    },
+  lastName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
 
-    lastName: {
-        type: String,
-        trim: true,
-        required: true
-    },
+  phone: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 11,
+    math: [/^\d{11}$/, "Phone number is not valid"],
+  },
 
-    phone: {
-        type: String,
-        trim: true,
-        required: true,
-        minlength: 11,
-    },
-})
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    validate: [
+      (email) => email.includes("@") && email.includes("."),
+      "Email is not valid",
+    ],
+  },
+
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  salary: {
+    type: Number,
+    default: 0,
+  },
+
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+
+  isLead: {
+    type: Boolean,
+    default: false,
+  },
+
+  startedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Personnel", PersonnelSchema);
