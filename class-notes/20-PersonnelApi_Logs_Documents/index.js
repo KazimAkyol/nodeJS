d6b1c2d68dfef1588e71bde87b34454e084c5b69
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-11 10:33:23 +0100

    add logger.js

--- class-notes/20-PersonnelApi_Logs_Documents/index.js
+++ class-notes/20-PersonnelApi_Logs_Documents/index.js
@@ -45,31 +45,42 @@ app.use(
 app.use(require("./src/middlewares/authentication"));
 
 // Logger
+app.use(require("./src/middlewares/logger"));
+
+/* ------------------------------------------------------- *
+// Logger
 // $ npm i morgan
 // $ https://expressjs.com/en/resources/middleware/morgan.html
 
-const morgan = require("morgan");
+const morgan = require('morgan');
 
 // app.use(morgan('tiny'));
-// app.use(morgan("short"));
-// app.use(morgan("dev"));
-// app.use(morgan("common"));
-// app.use(morgan("combined"));
+// app.use(morgan('short'));
+// app.use(morgan('dev'));
+// app.use(morgan('common'));
+// app.use(morgan('combined'));
 
 // Custom log:
-// app.use(
-//   morgan(
-//     'TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'
-//   )
-// );
+const customLog = 'TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'
+// app.use(morgan(customLog));
 
-// Write to file:
-const fs = require("node:fs");
-app.use(
-  morgan(customlog, {
-    stream: fs.createWriteStream("./examplelogs.log", { flags: "a+" }),
-  })
-);
+// Write to file;
+// const fs = require('node:fs');
+// app.use(morgan(customLog, {
+//     stream: fs.createWriteStream('./examplelogs.log', { flags: 'a+' })
+// }));
+
+
+// Write to file - Day by Day;
+const fs = require('node:fs');
+const now = new Date();
+// console.log(now);
+const today = now.toISOString().split('T')[0]
+// console.log(today)
+app.use(morgan(customLog, {
+    stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
+}));
+/* ------------------------------------------------------- */
 
 /* ------------------------------------------------------- */
 //* Routes:
