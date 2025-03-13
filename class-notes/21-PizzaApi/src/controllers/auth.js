0a66fd45546d46409829332eda9b749ac89b1fe5
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-13 11:50:40 +0100

    add auth.js-controller

--- class-notes/21-PizzaApi/src/controllers/auth.js
+++ class-notes/21-PizzaApi/src/controllers/auth.js
@@ -1,4 +1,43 @@
-"use strict"
+"use strict";
+
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const User = require('../controllers/user')
+
+module.exports = {
+
+    login: async (req,res) => {
+
+        if (!((username || email) && password)) {
+            res.errorStatusCode = 401
+            throw new Error('username/email and password are required.')
+        }
+
+        const user = await User.findOne({$or:[{username}, {email}], password })
+
+        if (!user) {
+            res.errorStatusCode = 401
+            throw new Error('Incorrect email/username and password.')
+        }
+
+        if (!user.isActive) {
+            res.errorStatusCode = 401
+            throw new Error('This account is not active.')
+        }
+
+        let tokenData = await
+
+        res.status(200).send({
+            error: false
+        })
+    },
+
+    logout: async (req,res) => {
+
+        res.status(200).send({
+            error: false
+        })
+    }
+}
