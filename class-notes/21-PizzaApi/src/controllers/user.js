a21787027ecebbecbb380c62ca5257228db840c1
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-13 09:53:29 +0100

    add users.js-controller

--- class-notes/21-PizzaApi/src/controllers/user.js
+++ class-notes/21-PizzaApi/src/controllers/user.js
@@ -1,4 +1,59 @@
-"use strict"
+"use strict";
+const { read, update } = require("../../../18-PersonnelApi/src/controllers/personnel.controller");
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const User = require("../models/user");
+
+module.exports = {
+    list: async (req,res) => {
+
+        const result = await
+
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+
+    create: async (req,res) => {
+
+        const result = await
+
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+
+    read: async (req,res) => {
+
+        const result = await
+
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+
+    update: async (req,res) => {
+
+        const result = await
+
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+
+    delete: async (req,res) => {
+
+        const result = await
+
+        res.status(200).send({
+            error: false,
+            result
+        })
+    }
+}
--- class-notes/21-PizzaApi/src/models/user.js
+++ class-notes/21-PizzaApi/src/models/user.js
@@ -3,6 +3,48 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
 
-const 
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: passwordEncrypt,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      index: true,
+      unique: true,
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Please enter a valid email address.",
+      ],
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { collection: "users", timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
