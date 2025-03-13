db7b99145407460f923e9078c98eabcfda1b340d
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-13 08:54:12 +0100

    add PizzaApi

--- class-notes/19-PersonnelApi_Token_Perms/src/routes/token.router.js
+++ class-notes/19-PersonnelApi_Token_Perms/src/routes/token.router.js
@@ -1,19 +1,20 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
-const token = require('../controllers/token.controller');
+const router = require("express").Router();
+const token = require("../controllers/token.controller");
 /* ------------------------------------------------------- */
 // URL: /tokens
 
-router.route('/').get(token.list).post(token.create);
+router.route("/").get(token.list).post(token.create);
 
-router.route('/:id')
-    .get(token.read)
-    .put(token.update)
-    .patch(token.update)
-    .delete(token.delete);
+router
+  .route("/:id")
+  .get(token.read)
+  .put(token.update)
+  .patch(token.update)
+  .delete(token.delete);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
