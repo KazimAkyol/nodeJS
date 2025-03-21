e92d0f30f0332777fa940dd38ef76d3994792506
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-21 10:08:34 +0100

    add user.js-model

--- class-notes/projects/flightAPI/src/models/user.js
+++ class-notes/projects/flightAPI/src/models/user.js
@@ -1,8 +1,9 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | Flight API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- *
 {
     "username": "test",
@@ -15,3 +16,35 @@ const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
 // User Model:
 
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: passwordEncrypt,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Please enter a valid email address.",
+      ],
+    },
+  },
+  {
+    collection: "user",
+    timestamps: true,
+  }
+);
