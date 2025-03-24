9b73ff4bca11607ccd79784cbbebbc5353fd03e7
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-24 10:47:23 +0100

    updated permissions.js

--- class-notes/23-PizzaApi_Email_Upload/src/middlewares/permissions.js
+++ class-notes/23-PizzaApi_Email_Upload/src/middlewares/permissions.js
@@ -1,17 +1,27 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 
 module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      //* Kisinin login olup/olmadigi ve active olup/olmadigi kontrol edilir.
+      next();
+      //* next() parametresiyle de bu kisinin bu isi yapip/yapamayacagi da belirtilmis olur.
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
+    }
+  },
 
-    isLogin: (req, res, next) => {
-
-        if (req.user && req.user.isActive) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login.')
-        }
-    },
-}
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      //* Kisinin admin olup/olmadigi ve active olup/olmadigi kontrol edilir.
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+};
