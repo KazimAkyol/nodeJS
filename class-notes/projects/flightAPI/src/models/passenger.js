cf5f50458e45b4b630793bf055b31392cf000a76
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-25 18:17:26 +0100

    add passenger.js-MCR

--- class-notes/projects/flightAPI/src/models/passenger.js
+++ class-notes/projects/flightAPI/src/models/passenger.js
@@ -1,4 +1,54 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | Flight API
 ------------------------------------------------------- */
+const { mongoose } = require("../configs/dbConnection");
+/* ---------------------------------------------------- */
+
+const PassengerSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    gender: {
+      type: String,
+      enum: [null, "M", "F"], //* sinirli sayida secenek sunulmak istendiginde enum degiskeni kullanilir.
+      default: null,
+      required: true,
+    },
+
+    emai: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Please enter a valid email address.",
+      ],
+    },
+
+    createdId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    collection: "passenger",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Passenger", PassengerSchema);
--- class-notes/projects/flightAPI/src/routes/passenger.js
+++ class-notes/projects/flightAPI/src/routes/passenger.js
@@ -1,4 +1,20 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | Flight API
 ------------------------------------------------------- */
+const router = require("express").Router();
+const passenger = require("../controllers/passenger");
+/* ------------------------------------------------------- */
+// URL: /passengers
+
+router.route("/").get(passenger.list).post(passenger.create);
+
+router
+  .route("/:id")
+  .get(passenger.read)
+  .put(passenger.update)
+  .patch(passenger.update)
+  .delete(passenger.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
